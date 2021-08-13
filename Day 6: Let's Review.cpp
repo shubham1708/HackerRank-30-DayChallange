7f388c4add4d94321623f0ee26aeaf28b35e76b2
shubham1708 <61815446+shubham1708@users.noreply.github.com>
2021-08-13 12:18:57 +0530

    Create Day 6: Let's Review.cpp

---
+++ Day 6: Let's Review.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+   int t; cin>>t;
+   while(t--){
+       string s;  cin>>s;
+       for(int i=0;i<s.length();i+=2){
+           
+               cout<<s[i];
+           
+       }cout<<" ";
+       for(int i=1;i<s.length();i+=2){
+           cout<<s[i];
+       }cout<<endl;
+   }
+}
