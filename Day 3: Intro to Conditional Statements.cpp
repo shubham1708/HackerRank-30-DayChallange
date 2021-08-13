ea70a0eefde8c97be9a9a3a54b4a9624622f662e
shubham1708 <61815446+shubham1708@users.noreply.github.com>
2021-08-13 12:15:59 +0530

    Create Day 3: Intro to Conditional Statements.cpp

---
+++ Day 3: Intro to Conditional Statements.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int main(){
+    int n;  cin>>n;
+    if(n%2!=0 || ((n%2==0 && n>=6 && n<=20))){
+        cout<<"Weird";
+    }else if(n%2==0 && ((n>=2 && n<=5) || (n>20))){
+        cout<<"Not Weird";
+    }else{
+        cout<<"Weird";
+    }
+}
